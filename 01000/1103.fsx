ded4bc1506c953094e35e1c4a09bc901ef5f891d
choshinyoung <choshinyoung1227@gmail.com>
2026-02-21 21:05:00 +0900

    1103

---
+++ 01000/1103.fsx
@@ -0,0 +1,28 @@
+open System
+
+let [| n; m |] = stdin.ReadLine().Split() |> Array.map int
+
+let board = Array.init n (fun _ -> stdin.ReadLine())
+let memo = Array2D.zeroCreate n m
+
+let rec dfs y x =
+    if x < 0 || y < 0 || x >= m || y >= n || board[y][x] = 'H' then
+        0
+    elif memo[y, x] = -1 then
+        -1
+    elif memo[y, x] <> 0 then
+        memo[y, x]
+    else
+        memo[y, x] <- -1
+
+        let d = int (board[y][x]) - int '0'
+
+        let next =
+            [ dfs y (x + d); dfs y (x - d); dfs (y - d) x; dfs (y + d) x ]
+
+        if List.forall (fun x -> x >= 0) next then
+            memo[y, x] <- (List.max next) + 1
+
+        memo[y, x]
+
+dfs 0 0 |> printfn "%d"
